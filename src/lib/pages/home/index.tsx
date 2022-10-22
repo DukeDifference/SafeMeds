d2494037aa34441b2cb73864c662246bdb575a36
Aaron Hsu <aaron.hsu@duke.edu>
2022-10-22 17:12:48 -0400

    feat(components): added screen cycle state hook and render

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,11 +1,27 @@
-import { Flex } from "@chakra-ui/react";
-import { NextSeo } from "next-seo";
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
 
-import CTASection from "lib/components/samples/CTASection";
+import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+
 import SomeImage from "lib/components/samples/SomeImage";
 import SomeText from "lib/components/samples/SomeText";
 
 const Home = () => {
+  const [screenID, setScreenID] = useState(0);
+
+  function retrieveScreen() {
+    switch (screenID) {
+      case 0:
+        return <SomeText />;
+      case 1:
+        return <SomeImage />;
+      default:
+        return <SomeText />;
+    }
+  }
+
   return (
     <Flex
       direction="column"
@@ -16,10 +32,7 @@ const Home = () => {
       mb={8}
       w="full"
     >
-      <NextSeo title="Home" />
-      <SomeText />
-      <SomeImage />
-      <CTASection />
+      {retrieveScreen()}
     </Flex>
   );
 };
