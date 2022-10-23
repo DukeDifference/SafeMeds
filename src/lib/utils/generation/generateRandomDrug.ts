fbcd40298028f123e44a1d7099ff6769a0e74d1b
Hadi Chaudhri <hadichaudhri@gmail.com>
2022-10-23 02:19:53 -0400

    feat(types): add rxcui and name fields to drugs table

--- src/lib/utils/generation/generateRandomDrug.ts
+++ src/lib/utils/generation/generateRandomDrug.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import type { Client } from "pg";
 import { v4 as uuidv4 } from "uuid";
 
@@ -5,6 +6,8 @@ import insertDrugToDb from "../db/insertDrugToDb";
 
 const generateRandomDrug = async (client: Client) => {
   const drugId = { id: uuidv4() };
-  return insertDrugToDb(client, drugId);
+  const rxcui = faker.datatype.number({ max: 10000 });
+  const name = faker.random.alpha(10);
+  return insertDrugToDb(client, drugId, rxcui, name);
 };
 export default generateRandomDrug;
